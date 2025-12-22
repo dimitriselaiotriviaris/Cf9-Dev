762e3e1c18644f23e3eacc1c8a855c1cc85f9eef
Dimitris <dimitris.elaiotriviaris@gmail.com>
2025-12-22 17:24:21 +0200

    Feat: ch17

---
+++ src/gr/aueb/cf/cf9/ch17/doubly_linked_list/Node.java
@@ -0,0 +1,35 @@
+package gr.aueb.cf.cf9.ch17.doubly_linked_list;
+
+public class Node<T> {
+    private T item;
+    private Node<T> prev;
+    private Node<T> next;
+
+    public Node(T item, Node<T> prev, Node<T> next) {
+        this.item = item;
+    }
+
+    public T getItem() {
+        return item;
+    }
+
+    public void setItem(T item) {
+        this.item = item;
+    }
+
+    public Node<T> getPrev() {
+        return prev;
+    }
+
+    public void setPrev(Node<T> prev) {
+        this.prev = prev;
+    }
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<T> next) {
+        this.next = next;
+    }
+}
